748e7f03d73794b6109ea83c3afd3e9c0f64b8e1
ShortyDev <shortydev8274@gmail.com>
2022-10-24 09:34:55 +0200

    würfelspiel

--- python/5_while_uebung1.py
+++ python/5_while_uebung1.py
@@ -1,7 +1,7 @@
 from random import randrange
 sum = 0
 while True:
-    num = randrange(20) + 10
+    num = randrange(10, 30)
     if num == 25 or num == 15:
        break
     sum += num
--- python/8_wuerfelspiel_uebung1.py
+++ python/8_wuerfelspiel_uebung1.py
@@ -1,5 +1,30 @@
 from random import randrange
 print("Willkommen zum Würfelspiel, mach dich bereit zum verlieren!")
 computer = []
+cSum = 0
 while len(computer) < 6:
     computer.append(randrange(6) + 1)
+    cSum += computer[-1]
+print("[1] Ja\n[2] Nein")
+start = int(input("Möchtest du starten?"))
+if start == 1:
+    wuerfe = 6
+    pSum = 0
+    while wuerfe > 0:
+        wuerfe -= 1
+        print("Schreibe x zum würfeln")
+        play = input()
+        if play == 'x':
+            wurf = randrange(6) + 1
+            pSum += wurf
+            print("Du hast eine", wurf, "gewürfelt! Der Computer hat eine", computer[6 - wuerfe - 1], "gewürfelt.")
+        else:
+            print("Dann nicht, jetzt bin ich beleidigt.")
+            break
+        if wuerfe == 0:
+            if cSum > pSum:
+                print("Du hast gegen den Computer verloren. Schade für dich.")
+            elif pSum > cSum:
+                print("Du hast gegen den Computer gewonnen. Gut gemacht, aber niemand ist stolz auf dich.")
+            else:
+                print("Fast. Computer und du sind gleich auf.")
